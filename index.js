97482a5a8f91cd78dc20ec6aa9782a3e465dce57
Lsong <song940@gmail.com>
2018-12-18 20:37:28 +0800

    add xml transform and transformFile

--- index.js
+++ index.js
@@ -13,14 +13,35 @@ class XML extends Transform {
   static async readFile(filename, options = {}){
     const readFile = promisify(fs.readFile);
     const source = await readFile(filename, 'utf8');
-    const dom = await XML.read(`<root>${source}</root>`, options);
-    return dom.children;
+    const ast = await XML.read(`<root>${source}</root>`, options);
+    return ast.children;
   }
   static serialize(ast, options){
     return printer(ast, options);
   }
   static traverse(ast, walker){
     return traverse(ast, walker);
+  }
+  static async transform(source, options = {}){
+    const { plugins = [] } = options;
+    var ast = source;
+    if(typeof source === 'string'){
+      ast = await XML.read(source);
+    }
+    const visitors = plugins
+      .map(plugin => plugin(ast))
+      .filter(Boolean);
+    XML.traverse(ast, node => {
+      visitors.forEach(visitor => {
+        const fn = visitor[node.name];
+        typeof fn === 'function' && fn(node);
+      });
+    });
+    return XML.serialize(ast, options);
+  }
+  static async transformFile(filename, options){
+    const ast = await XML.readFile(filename, options);
+    return XML.transform(ast, options);
   }
 }
 
